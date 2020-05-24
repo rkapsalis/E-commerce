80843ed1c2137659c28b1bcc9be8dd4ad954272f
rkapsalis <48930309+rkapsalis@users.noreply.github.com>
2020-05-24 18:49:03 +0300

    Update ar_shortcodes.php

--- ar_plugin/ar_shortcodes.php
+++ ar_plugin/ar_shortcodes.php
@@ -13,5 +13,15 @@ function arplugin_topViewedProducts_shortcode($atts){
      $content =  shortcode_create_topViewedProducts($atts['top_products']);
      return $content;
 }
-
+function arplugin_mostSimilarBought_shortcode($atts){
+      $a = shortcode_atts( 
+      	array(
+            'top_products' => '4',
+      	),
+      	$atts
+      );
+     $content =  shortcode_create_mostSimilarBought($atts['top_products']);
+     return $content;
+}
 add_shortcode('ar_shortcode1', 'arplugin_topViewedProducts_shortcode');
+add_shortcode('ar_shortcode2', 'arplugin_mostSimilarBought_shortcode');
